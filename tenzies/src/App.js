6aa9ac6620d1ee08ba26e7c4f0a8671b1a9af280
Victoria045 <victoriaberyl45@gmail.com>
2022-07-06 11:46:25 +0300

    mapping array of die to rtrieve individual die element

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,34 +1,47 @@
+import React, {useState} from "react";
 import './App.css'; 
 import Die from './components/Die';
-
+import {nanoid} from "nanoid";
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
 
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
       // newDice.push(Math.floor(Math.random()* 7) + 1)
-      newDice.push(Math.ceil(Math.random()* 6))
+      newDice.push({
+        value : Math.ceil(Math.random() * 6), 
+        isHeld : true,
+        id: nanoid()
+      })
     }
     return newDice
   }
-  console.log(allNewDice())
+  // console.log(allNewDice())
+
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />)
+
+  // useState((roll) => {
+  //   console.log("clicked")
+  // },[])
+
+  function rollDice() {
+
+    setDice(allNewDice())
+  }
+
+  
 
   return (
     <main>
       <div className="dice-wrap">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={1} />
-        <Die value={6} />
-        <Die value={3} />
-        <Die value={4 } />
-        <Die value={5} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {/* <Die value={diceElement} /> */}
+        {diceElements}
       </div>
-       
+      <button className="btn-roll" onClick={rollDice}>Roll</button>    
     </main>
   );
 }
--- tenzies/src/components/Die.js
+++ tenzies/src/components/Die.js
@@ -1,6 +1,9 @@
 export default function Die(props) {
+  const styles = {
+      backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
   return (
-    <div className="card">
+    <div className="card" style={styles}>
        <h2 className="card-elem">{props.value}</h2>
     </div>
   )
